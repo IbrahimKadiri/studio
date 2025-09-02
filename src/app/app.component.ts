4938cc252d1395ba207c0312a0e1b54e9d1eea74
Ibrahim <elkadiri.ibrahim@hotmail.com>
2025-09-02 12:24:05 +0200

    added gsap animation + stack component + svg icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,11 @@ import { ServicesComponent } from "./services/services.component";
 import { ProcessComponent } from "./process/process.component";
 import { PortfolioComponent } from "./portfolio/portfolio.component";
 import { ContactComponent } from "./contact/contact.component";
+import { StackComponent } from "./stack/stack.component";
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, ServicesComponent, ProcessComponent, PortfolioComponent, ContactComponent],
+  imports: [HeaderComponent, ServicesComponent, ProcessComponent, PortfolioComponent, ContactComponent, StackComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/stack/stack.component.ts
@@ -0,0 +1,57 @@
+import { CommonModule } from '@angular/common';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import gsap from 'gsap';
+
+@Component({
+  selector: 'app-stack',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './stack.component.html',
+  styleUrl: './stack.component.css'
+})
+export class StackComponent {
+  
+  logos = [
+    { name: 'Angular', src: 'assets/icons/angular.svg' },
+    { name: 'CSS3', src: 'assets/icons/css3.svg' },
+    { name: 'Git', src: 'assets/icons/git.svg' },
+    { name: 'GitHub', src: 'assets/icons/github.svg' },
+    { name: 'Heroku', src: 'assets/icons/heroku.svg' },
+    { name: 'HTML5', src: 'assets/icons/html5.svg' },
+    { name: 'JavaScript', src: 'assets/icons/javascript.svg' },
+    { name: 'MongoDB', src: 'assets/icons/mongodb.svg' },
+    { name: 'Node.js', src: 'assets/icons/nodejs.svg' },
+    { name: 'NPM', src: 'assets/icons/npm.svg' },
+    { name: 'PHP', src: 'assets/icons/php.svg' },
+    { name: 'React', src: 'assets/icons/react.svg' },
+    { name: 'RxJS', src: 'assets/icons/rxjs.svg' },
+    { name: 'Sass', src: 'assets/icons/sass.svg' },
+    { name: 'SQL', src: 'assets/icons/sql.svg' },
+    { name: 'TailwindCSS', src: 'assets/icons/tailwindcss.svg' },
+    { name: 'TypeScript', src: 'assets/icons/typescript.svg' },
+    { name: 'AWS', src: 'assets/icons/aws.svg' }
+  ];
+
+  @ViewChild('marqueeContainer') marqueeContainer!: ElementRef;
+  @ViewChild('marqueeContent') marqueeContent!: ElementRef;
+
+  ngAfterViewInit(): void {
+    this.initMarquee();
+  }
+
+  initMarquee() {
+    const container = this.marqueeContainer.nativeElement;
+    const content = this.marqueeContent.nativeElement;
+
+    const totalWidth = content.offsetWidth;
+
+    gsap.to(container.children, {
+      x: -totalWidth,
+      duration: 8,
+      ease: 'sine.inOut',
+      repeat: -1,
+      yoyo: true,
+    });
+  }
+
+}
